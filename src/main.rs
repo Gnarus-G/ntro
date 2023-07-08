dbb1be8d8be2104a65f6c4e1551df52794f69fb4
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-07-08 11:45:05 -0400

    feat: parse yaml and print type script type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,118 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, fs::File, io::BufReader, path::PathBuf};
+
+use clap::{Parser, Subcommand};
+use serde_yaml::Value;
+
+#[derive(Parser, Debug)]
+#[clap(author)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Command {
+    Yaml { file: PathBuf },
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Yaml { file } => {
+            let rdr = BufReader::new(File::open(&file)?);
+            let value: Value = serde_yaml::from_reader(rdr)?;
+
+            println!(
+                "type {} = {:#}",
+                file_name_to_type_name(
+                    file.file_stem()
+                        .expect("couldn't parse a filename from input")
+                        .to_str()
+                        .expect("path given should be in utf-8")
+                ),
+                introspect_typescript_types(value)
+            );
+        }
+    };
+
+    Ok(())
+}
+
+fn introspect_typescript_types(value: Value) -> String {
+    match value {
+        Value::Null => "null".to_string(),
+        Value::Bool(b) => b.to_string(),
+        Value::Number(n) => n.to_string(),
+        Value::String(s) => format!("'{s}'"),
+        Value::Sequence(s) => {
+            let mut buf = String::new();
+            buf.push('[');
+
+            let elements: Vec<_> = s.into_iter().map(introspect_typescript_types).collect();
+
+            buf.push_str(&elements.join(","));
+
+            buf.push(']');
+            buf
+        }
+        Value::Mapping(m) => {
+            let mut buf = String::new();
+            buf.push('{');
+
+            let kvs: Vec<_> = m
+                .into_iter()
+                .map(|(key, value)| {
+                    format!(
+                        "{}: {}",
+                        &introspect_typescript_types(key),
+                        &introspect_typescript_types(value)
+                    )
+                })
+                .collect();
+
+            buf.push_str(&kvs.join(","));
+
+            buf.push('}');
+            buf
+        }
+        Value::Tagged(tv) => introspect_typescript_types(tv.value),
+    }
+}
+
+fn file_name_to_type_name(fname: &str) -> String {
+    fname
+        .split(['-', '.'])
+        .map(to_first_uppercase)
+        .collect::<Vec<_>>()
+        .join("")
+}
+
+fn to_first_uppercase(n: &str) -> String {
+    let mut buf = n.to_owned();
+    let fc = buf.get(0..1).unwrap_or_default().to_owned().to_uppercase();
+    buf.replace_range(0..1, &fc);
+    buf
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::file_name_to_type_name;
+
+    #[test]
+    fn file_name_to_type_name_conversion() {
+        assert_eq!(file_name_to_type_name("test"), "Test".to_string());
+        assert_eq!(
+            file_name_to_type_name("test.config"),
+            "TestConfig".to_string()
+        );
+        assert_eq!(
+            file_name_to_type_name("test-config"),
+            "TestConfig".to_string()
+        );
+        assert_eq!(
+            file_name_to_type_name("test-config-tee.prod"),
+            "TestConfigTeeProd".to_string()
+        );
+    }
 }
