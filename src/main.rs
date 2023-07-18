375acb5fae1c5218423c479270d3eb4834f9a2c2
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-07-17 22:30:40 -0400

    feat: detect package manager and call appropriate executor for prettier

--- src/main.rs
+++ src/main.rs
@@ -115,15 +115,58 @@ fn prettify(file: &[u8], file_name: &Path) -> Result<Vec<u8>, Box<dyn Error>> {
     }
 }
 
-fn prettier(file_name: &Path) -> Result<process::Child, std::io::Error> {
+fn prettier(file_name: &Path) -> Result<process::Child, Box<dyn Error>> {
+    let pm = PackageManager::get();
+
+    let package_manager_executable = pm
+        .map(PackageManager::into_exe_name)
+        .map(|exe| (exe, vec!["prettier", "--stdin-filepath"]))
+        .ok_or::<String>("no executable found with which to run prettier".into());
+
     let (exe, args) = which("prettierd")
         .map(|_| ("prettierd", vec![]))
-        .unwrap_or(("npx", vec!["prettier", "--stdin-filepath"]));
+        .or(package_manager_executable)?;
 
-    process::Command::new(exe)
+    let child = process::Command::new(exe)
         .args(args)
         .arg(file_name)
         .stdin(process::Stdio::piped())
         .stdout(process::Stdio::piped())
-        .spawn()
+        .spawn()?;
+
+    Ok(child)
+}
+
+enum PackageManager {
+    Pnpm,
+    Yarn,
+    Npm,
+}
+
+impl PackageManager {
+    fn get() -> Option<Self> {
+        let Ok(dir) = std::env::current_dir() else {
+            return None;
+        };
+
+        if dir.join("pnpm-lock.yaml").is_file() {
+            return Some(Self::Pnpm);
+        }
+        if dir.join("yarn.lock").is_file() {
+            return Some(Self::Yarn);
+        }
+        if dir.join("package-lock.json").is_file() {
+            return Some(Self::Npm);
+        }
+
+        None
+    }
+
+    fn into_exe_name(self) -> &'static str {
+        match self {
+            PackageManager::Pnpm => "pnpx",
+            PackageManager::Yarn => "yarn",
+            PackageManager::Npm => "npx",
+        }
+    }
 }
